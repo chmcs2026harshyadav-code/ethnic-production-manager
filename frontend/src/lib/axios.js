66f4dbeaddcd74498684ae32eaaa4497f127d737
chmcs2026harshyadav-code <chmcs2026.harsh.yadav@gmail.com>
2026-03-06 19:37:52 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://ethnic-production-manager.onrender.com/products",
+  baseURL: "https://ethnic-production-manager.onrender.com",
 });
 
 export default instance;
